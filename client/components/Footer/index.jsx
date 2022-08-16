79ebd418d150af5d4a329e8635667b29d3c838b5
Khalid <sudjud@bk.ru>
2022-08-16 10:19:52 +0300

    Добавлены некоторые папки и файлы

---
+++ client/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import footer from './footer.style.sass';
+
+function Footer() {
+
+  return (
+    <div className={footer.footer}>
+
+    </div>
+  )
+};
+export default Footer;
---
+++ client/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import header from './header.module.sass';
+
+function Header() {
+
+  return (
+    <div className={header.header}>
+
+    </div>
+  )
+};
+export default Header;
