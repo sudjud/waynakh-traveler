2f4b56efa470b09f232230d6f00494118dd6ba19
Khamzaatov <adamkhamzatov9522@gmail.com>
2022-08-16 19:53:00 +0300

    Сделал авторизацию и регистрацию

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import place from "../features/placeSlice";
+
 
 export const store = configureStore({
     reducer : {
-        
+        place
     }
 })
--- client/src/pages/Auth/signIn.jsx
+++ client/src/pages/Auth/signIn.jsx
@@ -1,13 +1,38 @@
-const signIn = () => {
+import auth from './auth.module.sass'
+import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai'
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+const SignIn = () => {
+    const [login, setLogin] = useState('')
+    const [password, setPassword] = useState('')
+    const [passwordShown, setPasswordShown] = useState(false);
+
+    const handleSetLogin = (e) => {
+        setLogin(e.target.value)
+    }
+
+    const handleSetPass = (e) => {
+        setPassword(e.target.value)
+    }
+
+    const togglePassword = () => {
+        setPasswordShown(!passwordShown);
+      };
+
     return (
-        <div className=''>
-        {/* #009394 #2a3950 */}
+        <div className={auth.auth}>
             <form>
-                <input type='text' placeholder="E-mail" />
-                fsdpfspdfnpsdfnpdfn
+                <h1>Вход</h1>
+                <input type='text' value={login} onChange={handleSetLogin} placeholder="Имя пользователя" />
+                <div style={{display : 'flex'}}><input type={passwordShown ? 'text' : 'password'} value={password} onChange={handleSetPass} placeholder="Пароль"></input>
+                {passwordShown ? <AiFillEyeInvisible  className={auth.eye} onClick={togglePassword}/> : 
+                    <AiFillEye className={auth.eye} onClick={togglePassword}/>}</div>
+                <button>Войти</button>
             </form>
+            <p>Ещё нет аккаунта? <Link to='/auth'>Зарегистрируйтесь</Link></p>
         </div>
     );
 };
 
-export default signIn;
+export default SignIn;
