5a0fc870e6f8c99ff56e7efbe4d03d12120d7d07
Umar95 <malsagov_umar@mail.ru>
2022-08-16 14:55:11 +0300

    sdelal

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -4,8 +4,28 @@ function Footer() {
 
   return (
     <div className={footer.footer}>
-
+      <div className={footer.logo}>
+        <img src="./logo.png" alt="" />
+      </div>
+      <div className={footer.about}>
+        <ul>
+          <h3>About</h3>
+          <li>aaaa</li>
+          <li>aaaa</li>
+          <li>aaaa</li>
+          <li>aaaa</li>
+        </ul>
+      </div>
+      <div className={footer.company}>
+        <ul>
+          <h3>Company</h3>
+          <li>aaaa</li>
+          <li>aaaa</li>
+          <li>aaaa</li>
+          <li>aaaa</li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 };
 export default Footer;
