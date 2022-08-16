ded4d556ccd3dbca70b88b342db32deb6666b61d
Khalid <sudjud@bk.ru>
2022-08-16 19:50:23 +0300

    Сделана модель place

--- backend/models/Place.model.js
+++ backend/models/Place.model.js
@@ -1,5 +1,17 @@
 const mongoose = require('mongoose');
 
 const placeSchema = mongoose.Schema({
-  
+  name: { type: String, require: true },
+  point: { type: String, require: true },
+  description: String,
+  likes: [
+    {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'User'
+    }
+  ],
+  comments: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: 'Comment'
+  }
 })
