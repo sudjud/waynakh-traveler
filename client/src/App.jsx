e5acee3bfea1388e89dc7bda570af1c68474caef
Malsagov <rus.malsagov177@mail.ru>
2022-08-18 18:46:20 +0300

    Добавил чуть карту))

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,20 @@
-import './style.sass';
-import Header from './components/Header';
-import { Routes, Route } from 'react-router-dom';
-import Footer from './components/Footer';
-import SignUp from './pages/Auth/SignUp';
-import SignIn from './pages/Auth/SignIn';
-
+import "./style.sass";
+import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import SignUp from "./pages/Auth/SignUp";
+import SignIn from "./pages/Auth/SignIn";
+import YandexMap from "./components/YandexMap/index";
 
 function App() {
   return (
     <div className="app">
       <Header />
-        <Routes>
-          <Route path='/login' element={<SignIn />}></Route>
-          <Route path='/auth' element={<SignUp />}></Route>
-        </Routes>
+      <Routes>
+        <Route path="/login" element={<SignIn />}></Route>
+        <Route path="/auth" element={<SignUp />}></Route>
+        <Route path="/map" element={<YandexMap />}></Route>
+      </Routes>
     </div>
   );
 }
