b178fac2421511c3b607c5886227e1fe840fdcab
Malsagov <rus.malsagov177@mail.ru>
2022-08-19 16:54:33 +0300

    карта

--- client/src/components/YandexMap/MapBalloon.jsx
+++ client/src/components/YandexMap/MapBalloon.jsx
@@ -1,19 +1,15 @@
 import React from "react";
 import map from "./map.module.sass";
 import { Placemark } from "@pbe/react-yandex-maps";
-import imgg from "./1.jpg";
-// import { useEffect } from "react";
-// import { createPortal } from "react-dom";
+
 const MapBalloon = ({ name, point, areas, photos }) => {
   const [point_, point_2] = point.split(" ");
   const point_1 = point_.slice(0, point_.length - 1);
-  console.log(photos[0].name);
+  console.log(`http://localhost:3030/${photos[0].name}`);
 
   return (
     <div>
-      {/* <img
-        src={require(`../../../../backend/uploads/images/${photos[0].name}`)}
-      ></img> */}
+
       <Placemark
         options={{ iconColor: "green" }}
         geometry={[+point_1, +point_2]}
